5465545de60bb50212381ec7ccb222b6508fce3e
Ivan Buendia <ivanbuendiaguillen@gmail.com>
2017-11-23 10:44:25 +0100

    usuario a lista creada

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -1,11 +1,26 @@
 'use strict';
 
-module.exports = function(Listafamiliar) {
-Listafamiliar.beforeRemote('create', function(context, Listafamiliar, next){
-    
-    context.args.data.owner= context.req.accessToken.userId; 
-    next();
-});
+module.exports = function (ListaFamiliar) {
+    ListaFamiliar.beforeRemote('create', function (context, Listafamiliar, next) {
 
+        context.args.data.owner = context.req.accessToken.userId;
+        next();
+    });
+    ListaFamiliar.afterRemote('create', function (context, Listafamiliar, next) {
+        var app = ListaFamiliar.app;
+        var userId = context.req.accessToken.userId;
+        var id = context.args.data.listaFamiliarId;
+
+        
+        var Usuario = app.models.Usuario;
+        console.log(Usuario);
+        Usuario.findById(userId, function(err, usuario){
+        if(err) next(err)
+        usuario.listaFamiliarId=Listafamiliar.id;
+        usuario.save();
+        next();
+    })
+        
+    })
 
 };
