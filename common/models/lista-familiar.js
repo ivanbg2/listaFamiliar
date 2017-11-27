0d2dd3960619f2f5a5ac5f832fe7922792262b26
Ivan Buendia <ivanbuendiaguillen@gmail.com>
2017-11-27 08:38:02 +0100

    crear solicitudes

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -11,16 +11,48 @@ module.exports = function (ListaFamiliar) {
         var userId = context.req.accessToken.userId;
         var id = context.args.data.listaFamiliarId;
 
-        
+
         var Usuario = app.models.Usuario;
         console.log(Usuario);
-        Usuario.findById(userId, function(err, usuario){
-        if(err) next(err)
-        usuario.listaFamiliarId=Listafamiliar.id;
-        usuario.save();
-        next();
-    })
+        Usuario.findById(userId, function (err, usuario) {
+            if (err)
+                next(err)
+            usuario.listaFamiliarId = Listafamiliar.id;
+            usuario.save();
+            next();
+        });
+
+    });
+    //Debemos crear un punto de entrada POST /ListasFamiliares/{id}/solicitar, que inserte en la 
+    //tabla de solucitudes una fila que contenga el id de la lista familiar a la que se quiere 
+    //pertenecer y el id del usuario que está autenticado.
+
+    /**
+     * inserta tabla de solicitudes id
+     * @param {Function(Error, object)} callback
+     */
+
+    ListaFamiliar.prototype.solicitar = function (context, callback) {
+        var salida;
+        var userId = context.req.accessToken.userId;
+        var lista = this;
+        var Usuario = ListaFamiliar.app.models.Usuario;
+
+        Usuario.findById(userId, function (err, pepe) {
+            if (err)
+                callback(err);
+            lista.solicitudes.add(pepe,
+                    function (err, vuelta) {
+                        salida=vuelta;
+                        callback(null, salida);
+                    });
+        });
+
+
         
-    })
+        
+    };
 
 };
+
+          
